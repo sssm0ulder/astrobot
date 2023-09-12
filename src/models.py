21d11d533d721f0ffb0c0e93051e755250f38b5d
Ax3 <sssm0ulder@gmail.com>
2023-09-12 17:13:20 +0300

    feat(models): add SubscriptionPeriod callback data model

--- src/models.py
+++ src/models.py
@@ -3,4 +3,7 @@ from aiogram.filters.callback_data import CallbackData
 
 class DateModifier(CallbackData, prefix='date_modifier'):
     modifier: int
+
+class SubscriptionPeriod(CallbackData, prefix='sub_period'):
+    months: int
     
