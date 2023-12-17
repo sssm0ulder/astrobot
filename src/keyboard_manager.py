c957ae1a5e1cfd6bfd89f7c94ab8450e50df1e46
Ax3 <sssm0ulder@gmail.com>
2023-12-17 10:33:28 +0200

    кнопка Общий сверху в луне в знаке

--- src/keyboard_manager.py
+++ src/keyboard_manager.py
@@ -290,8 +290,8 @@ class KeyboardManager:
 
         self.moon_in_sign_menu = self.build_keyboard_from_structure(
             [
-                [bt.favorable, bt.unfavorable],
                 [bt.general],
+                [bt.favorable, bt.unfavorable],
                 [bt.main_menu]
             ],
             is_inline=True
