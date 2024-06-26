ad186b89b65e8ffc4ccc4caaa5cfa1174b990732
sssm0ulder <sssm0ulder@example.com>
2024-06-26 16:12:23 +0300

    bugfix

--- src/common.py
+++ src/common.py
@@ -2,7 +2,7 @@ import os
 import dotenv
 
 from aiogram import Bot
-
+from aiogram.client.default import DefaultBotProperties
 from src.utils import get_day_selection_database
 
 
@@ -10,7 +10,7 @@ dotenv.load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(BOT_TOKEN, parse_mode='html')
+bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode='html'))
 
 DAY_SELECTION_DATABASE: dict[str, dict] = get_day_selection_database()
 
